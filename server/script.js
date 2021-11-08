50b70e7072b5746a9ded3fbf357c8a4d54623c9a
Yassine Ibhir <yassine.ibhir@dawsoncollege.qc.ca>
2021-11-08 14:30:40 -0500

    Add comments

--- server/script.js
+++ server/script.js
@@ -4,10 +4,15 @@
 
 const insertToDb = require("./utils/writeToDb");
 
+// DataBase name
 const dbName = "parkings";
 
+// Collection Name
 const collName = "parking_streets";
 
+/**
+ * Async IIFE populate dataset. 
+ */
 (async function(){
 
     await insertToDb(dbName,collName);
