0f8896ad5a50eb7bc5cd916dc829c85e8fde03fd
Yassine Ibhir <yassine.ibhir@dawsoncollege.qc.ca>
2021-11-08 14:15:01 -0500

    implement writeTodb

--- server/utils/writeToDb.js
+++ server/utils/writeToDb.js
@@ -3,3 +3,44 @@
  * Use conn.js to insert the array of documents in mongodb
  * Create index geo.
  */
+
+
+// Path of data set
+const path = "./server/utils/montreal_parking_streets.json";
+
+//Load module
+const readData = require("./load");
+
+// Database module
+const Dao = require("../db/conn");
+
+/**
+ * Access load.js to get an array of documents.
+ * Connet to database and InsertMany.
+ * @param {String} dbName 
+ * @param {string} colName 
+ */
+async function insertToDb(dbName, colName){
+
+    // Read and get documents
+    let documents = await readData(path);
+
+    // Create Dao instance
+    let dao = new Dao();
+
+    // Connect to database
+    await dao.connect(dbName, colName);
+    
+    // Insert documents
+    await dao.insertMany(documents);
+    
+    // Close connection
+    await dao.close();
+
+}
+
+(async function(){
+    await insertToDb("parkings", "parking_streets");
+})();
+
+module.exports = insertToDb;
