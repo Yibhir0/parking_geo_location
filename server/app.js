457b1599206aca6c983f611b6eb9c4e3ed89ed5e
Yassine Ibhir <yassine.ibhir@dawsoncollege.qc.ca>
2021-11-15 13:13:28 -0500

    Experimenting with express routes

--- server/app.js
+++ server/app.js
@@ -1,3 +1,14 @@
 /**
   * Link to all endPoint
  */
+ "use srict"
+
+ const express = require("express");
+ const app = express();
+ const path = require("path");
+ 
+
+ app.get("/api", api);
+  
+
+ module.exports = app;
--- server/server.js
+++ server/server.js
@@ -2,3 +2,36 @@
  * Main file run express server 
  * const PORT = process.env.PORT || 3001
  */
+
+ "use strict"
+ const app = require("./app");
+ const Dao = require("./db/conn");
+ const dbName = "parkings";
+
+// Collection Name
+const collName = "parking_streets";
+
+ (async function(){
+    
+ try{
+   
+
+    // Create Dao instance
+    let dao = new Dao();
+
+    // Connect to database
+    await dao.connect(dbName, collName);
+    
+    // Start server when database connection is set
+    app.listen(3000, () => {
+        console.log("Server listening on port 3000!");
+      });
+  
+  } catch(err){
+    console.error(err);
+  }
+ 
+})();
+
+
+ 
