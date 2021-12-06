fd86f4f569543ef1fb069734405d763d95e50756
EMS7 <rony-estefan.maheux-saban@dawsoncollege.qc.ca>
2021-12-06 12:26:03 -0500

    add build in gitlab-ci.yml

---
+++ client/src/utils/config.js
@@ -0,0 +1,3 @@
+const attribution = 
+    "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors";
+const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
