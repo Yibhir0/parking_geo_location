84e3806c638ef6156ece654fe2d396aeb6a30228
yassine ibhir <yibhir101@gmail.com>
2021-12-07 14:56:54 -0500

    complete map display

--- client/src/components/ParkingMap.js
+++ client/src/components/ParkingMap.js
@@ -1,9 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React, { Component } from "react";
-import { 
-  MapContainer, 
-  TileLayer, 
-  CircleMarker, 
-  Popup
+import {
+  MapContainer,
+  TileLayer,
+  CircleMarker,
 } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-markercluster";
 import "leaflet/dist/leaflet.css";
@@ -11,40 +11,64 @@ import "react-leaflet-markercluster/dist/styles.min.css";
 
 class ParkingMap extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
-      points : [],
-      selected : null, 
+      points: [[45.5017, -73.5673]],
+      selected: null,
     }
   }
 
   render() {
     return (
-      <MapContainer
-        center = {this.props.center}
-        zoom = {this.props.zoom}
-        minZoom = {this.props.minZoom}
-        maxZoom = {this.props.maxZoom}
-        zoomCntrol = {false}
-        updateWhenZooming = {false}
-        updateWhenIdle = {true}
-        preferCanvas = {true}
-        style = {{
-          width: "100%",
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          zIndex: -1
-        }}
+      <div>
+        <MapContainer
+          center={this.props.center}
+          zoom={this.props.zoom}
+          minZoom={this.props.minZoom}
+          maxZoom={this.props.maxZoom}
+          zoomControl={false}
+          updateWhenZooming={false}
+          updateWhenIdle={true}
+          preferCanvas={true}
+          style={{
+            width: "100%",
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            zIndex: -1
+          }}
+        >
+          <TileLayer
+            url={this.props.tileUrl}
+            attribution={this.props.attribution} />
+          <MarkerClusterGroup
+            spiderfyOnMaxZoom={false}
+            zoomToBoundsOnClick={true}
+            showCoverageOnHover={true}
+            removeOutsideVisibleBounds={false}
+            disableClusteringAtZoom={this.props.maxZoom}
+          >
+            {this.state.points.map((item, index) =>
+              <CircleMarker
+                key={index}
+                color={"red"}
+                opacity={1}
+                radius={5}
+                weight={1}
+                center={[45.5017, -73.5673]}
+              // center={item.geometry.coordinates}
+              // eventHandlers={{
+              //   click: () => {
+              //     this.setState({ selected: item });
+              //   }
+              // }} 
+              />
+            )}
 
-      >
-        <TileLayer />
-        <MarkerClusterGroup>
-         
-          <CircleMarker />
-        </MarkerClusterGroup>
-      </MapContainer>
+          </MarkerClusterGroup>
+        </MapContainer>
+      </div>
     )
   }
 
--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -3,17 +3,20 @@
 /**
  * Map configuration variables
  */
+
 const MapConfig = {
-  attribution : "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
-  tileUrl : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-  center : [-73.6111, 45.4444],
-  bounds : [
+
+  // eslint-disable-next-line max-len
+  attribution: "&copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
+  tileUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+  center: [45.5017, -73.5673],
+  bounds: [
     [-73.9890, 45.9111],
     [-73.1200, 45.2222]
   ],
-  minZoom : 1,
-  maxZoom : 18,
-  intitialZoom : 0  
+  minZoom: 1,
+  maxZoom: 18,
+  intitialZoom: 8
 }
 
 export default MapConfig;
